1e0c6314022f65ff0b45480714fd46ae76472adc
Adam Wathan <adam.wathan@gmail.com>
2015-04-01 12:45:47 -0400

    Update package references

--- src/AdamWathan/EloquentOAuthL4/EloquentOAuthServiceProvider.php
+++ src/AdamWathan/EloquentOAuthL4/EloquentOAuthServiceProvider.php
@@ -2,16 +2,27 @@
 
 use Illuminate\Support\ServiceProvider;
 use GuzzleHttp\Client as HttpClient;
+use SocialNorm\SocialNorm;
+use SocialNorm\ProviderRegistry;
+use SocialNorm\Request;
+use SocialNorm\StateGenerator;
+use AdamWathan\EloquentOAuth\Authenticator;
+use AdamWathan\EloquentOAuth\IdentityStore;
+use AdamWathan\EloquentOAuth\Session;
+use AdamWathan\EloquentOAuth\OAuthIdentity;
+use AdamWathan\EloquentOAuth\OAuthManager;
+use AdamWathan\EloquentOAuth\UserStore;
 
 class EloquentOAuthServiceProvider extends ServiceProvider {
 
-    protected $providerLookup = array(
-        'facebook' => 'AdamWathan\\EloquentOAuth\\Providers\\FacebookProvider',
-        'github' => 'AdamWathan\\EloquentOAuth\\Providers\\GitHubProvider',
-        'google' => 'AdamWathan\\EloquentOAuth\\Providers\\GoogleProvider',
-        'linkedin' => 'AdamWathan\\EloquentOAuth\\Providers\\LinkedInProvider',
-        'instagram' => 'AdamWathan\\EloquentOAuth\\Providers\\InstagramProvider',
-    );
+    protected $providerLookup = [
+        'facebook' => 'SocialNorm\Facebook\FacebookProvider',
+        'github' => 'SocialNorm\GitHub\GitHubProvider',
+        'google' => 'SocialNorm\Google\GoogleProvider',
+        'linkedin' => 'SocialNorm\LinkedIn\LinkedInProvider',
+        'instagram' => 'SocialNorm\Instagram\InstagramProvider',
+        'soundcloud' => 'SocialNorm\SoundCloud\SoundCloudProvider',
+    ];
 
     /**
      * Indicates if loading of the provider is deferred.
@@ -27,7 +38,7 @@ class EloquentOAuthServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        $this->package('adamwathan/eloquent-oauth');
+        $this->package('adamwathan/eloquent-oauth-l4');
     }
 
     /**
@@ -43,32 +54,39 @@ class EloquentOAuthServiceProvider extends ServiceProvider {
     protected function registerOAuthManager()
     {
         $this->app['adamwathan.oauth'] = $this->app->share(function ($app) {
-            $this->configureOAuthIdentitiesTable();
+            $providerRegistry = new ProviderRegistry;
+            $session = new Session($app['session']);
+            $request = new Request($app['request']->all());
+            $stateGenerator = new StateGenerator;
+            $socialnorm = new SocialNorm($providerRegistry, $session, $request, $stateGenerator);
+            $this->registerProviders($socialnorm, $request);
+
             $users = new UserStore($app['config']['auth.model']);
-            $stateManager = new StateManager($app['session.store'], $app['request']);
-            $authorizer = new Authorizer($app['redirect']);
-            $authenticator = new Authenticator($app['auth'], $users, new IdentityStore);
-            $oauth = new OAuthManager($authorizer, $authenticator, $stateManager, new ProviderRegistrar);
-            $this->registerProviders($oauth);
+            $authenticator = new Authenticator($app['Illuminate\Contracts\Auth\Guard'], $users, new IdentityStore);
+
+            $oauth = new OAuthManager($app['redirect'], $authenticator, $socialnorm);
             return $oauth;
         });
     }
 
-    protected function registerProviders($oauth)
+    protected function registerProviders($socialnorm, $request)
     {
-        $providerAliases = $this->app['config']['eloquent-oauth::providers'];
+        if (! $providerAliases = $this->app['config']['eloquent-oauth-l4::providers']) {
+            return;
+        }
+
         foreach ($providerAliases as $alias => $config) {
-            if(isset($this->providerLookup[$alias])) {
+            if (isset($this->providerLookup[$alias])) {
                 $providerClass = $this->providerLookup[$alias];
-                $provider = new $providerClass($config, new HttpClient, $this->app['request']);
-                $oauth->registerProvider($alias, $provider);
+                $provider = new $providerClass($config, new HttpClient, $request);
+                $socialnorm->registerProvider($alias, $provider);
             }
         }
     }
 
     protected function configureOAuthIdentitiesTable()
     {
-        OAuthIdentity::configureTable($this->app['config']['eloquent-oauth::table']);
+        OAuthIdentity::configureTable($this->app['config']['eloquent-oauth-l4::table']);
     }
 
     /**
